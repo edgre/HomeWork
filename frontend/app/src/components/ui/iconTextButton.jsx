398783a97c39a7bad553f773027f9ed86f45fde1
edgre <egorprudnikov71@gmail.com>
2025-06-30 17:39:59 +0700

    Изменение в структуре проекта

--- frontend/app/src/components/ui/iconTextButton.jsx
+++
@@ -1,9 +0,0 @@
-function ButtonWithIcon({ icon, children, onClick, className }) {
-  return (
-    <button onClick={onClick} className={className}>
-      <img src={icon} />
-      <span style={{ textDecoration: "none" }}>{children}</span>
-    </button>
-  );
-}
-export default ButtonWithIcon;
--- frontend/app/src/components/ui/iconTextLabel.jsx
+++
@@ -1,9 +0,0 @@
-function LabelWithIcon({ icon, children, className }) {
-  return (
-    <label className={className}>
-      <img src={icon} />
-      <span>{children}</span>
-    </label>
-  );
-}
-export default LabelWithIcon;
